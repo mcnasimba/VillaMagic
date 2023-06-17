8e8ef9b3d72313684d1ecf9056974353a2df8162
Cristhian Nasimba <mcnasimba@uce.edu.ec>
2023-06-16 22:10:38 -0500

    Gestion Villa

---
+++ MagicVilla_API/Datos/VillaStore.cs
@@ -0,0 +1,14 @@
+﻿using MagicVilla_API.Modelos.DTO;
+
+namespace MagicVilla_API.Datos
+{
+    public static class VillaStore
+    {
+
+        public static List<VillaDTO> villaList = new List<VillaDTO>
+        {
+            new VillaDTO {Id=1, Nombre="Cris"},
+            new VillaDTO {Id=2,Nombre="Andres"}
+        }; 
+    }
+}
